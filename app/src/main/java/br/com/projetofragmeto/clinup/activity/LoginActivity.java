1c309fea8ec294851900636ccca5cb1fb88740e4
Ihago Santos <ihagog12@gmail.com>
2018-06-20 14:00:29 -0300

    Mudança das cores da interface

--- app/src/main/java/br/com/projetofragmeto/clinup/activity/LoginActivity.java
+++ app/src/main/java/br/com/projetofragmeto/clinup/activity/LoginActivity.java
@@ -108,23 +108,6 @@ public class LoginActivity extends AppCompatActivity{
         callbackManager = CallbackManager.Factory.create();
         clickBotaoFacebook();
 
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "br.com.projetofragmeto.clinup.activity",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-                Log.i("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-
-        } catch (NoSuchAlgorithmException e) {
-
-        }
-
 
 
 
