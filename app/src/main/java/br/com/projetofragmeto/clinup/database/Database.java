63a913f7d5a0b2bd788ad27a9593d55aee2a0936
Ihago Santos <ihagog12@gmail.com>
2018-07-16 22:36:04 -0300

    classes ClinicaDB, HospitalDB, LaboratorioDB e ProfissionalDB que implementam a interface Database. Essas classes são responsáveis por fazer as consultas no banco

---
+++ app/src/main/java/br/com/projetofragmeto/clinup/database/Database.java
@@ -0,0 +1,9 @@
+package br.com.projetofragmeto.clinup.database;
+
+public interface Database {
+
+    public void adicionarDados();
+    public void removerDados();
+    public void buscarDados();
+    public void alterarDados();
+}
---
+++ app/src/main/java/br/com/projetofragmeto/clinup/database/HospitalDB.java
@@ -0,0 +1,23 @@
+package br.com.projetofragmeto.clinup.database;
+
+public class HospitalDB implements Database {
+    @Override
+    public void adicionarDados() {
+
+    }
+
+    @Override
+    public void removerDados() {
+
+    }
+
+    @Override
+    public void buscarDados() {
+
+    }
+
+    @Override
+    public void alterarDados() {
+
+    }
+}
