fc1e02ac6a89998818a8382ab28009fcc76de7d8
AndersonMelo123 <anderson.ufrpe@gmail.com>
2018-08-28 15:30:28 -0300

    Alteração do icone de perfil do usuario

--- app/src/main/java/br/com/projetofragmeto/clinup/fragments/MapaFragment.java
+++ app/src/main/java/br/com/projetofragmeto/clinup/fragments/MapaFragment.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -252,7 +253,7 @@ public class MapaFragment extends Fragment implements
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
- /*       mView = inflater.inflate(R.layout.fragment_mapa, container, false);
+        mView = inflater.inflate(R.layout.fragment_mapa, container, false);
 
         FloatingActionButton fab = mView.findViewById(R.id.botaoBuscar); // Botão flutuante que leva para tela de busca
 
@@ -262,7 +263,7 @@ public class MapaFragment extends Fragment implements
                 goToBuscaActivity();
             }
         });
-        */
+
         return mView;
     }
 
